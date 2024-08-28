70f99cfea0420a0d6f4ed6a826d9d5e7cd0845a2
afterhourss <adiptabagasss@gmail.com>
2024-08-28 16:37:29 +0700

    change link and pass type props

--- src/components/Projectbar.jsx
+++ src/components/Projectbar.jsx
@@ -1,7 +1,7 @@
 import { MdOutlineArrowOutward } from "react-icons/md";
 import Tag from "./Tag";
 
-export default function Projectbar({projectName, subTitle, image, desc, source}) {
+export default function Projectbar({projectName, subTitle, image, desc, source, type}) {
   return (
     <div className="text-white lg:h-[250px] flex flex-col lg:flex-row bg-[#141414] border border-[#292929] brightness-75 hover:brightness-100 transition-all">
         <img src={image} alt="" className="object-cover lg:rounded-l-md w-full"/>
@@ -12,7 +12,7 @@ export default function Projectbar({projectName, subTitle, image, desc, source})
             <div className="text-[#5B5B5B] text-justify">{desc}</div>
           </div>
             <div className="flex justify-between">
-                <Tag text="Website"/>
+                <Tag text={type}/>
                 <a href={source} target="_blank">
                   <MdOutlineArrowOutward className="text-[#5B5B5B] hover:text-[#9C9C9C] transiton-colors text-3xl cursor-pointer"/>
                 </a>
